2462515ed9dd2e1dbf831dfac58bb1b10e146782
Stephanie <stephanierc16@hotmail.com>
2014-11-11 21:59:12 -0200

    CRUD Hospedagem e Venda de Ingresso

--- src/br/senai/sc/hoteleclipse/entity/Hospedagem.java
+++ src/br/senai/sc/hoteleclipse/entity/Hospedagem.java
@@ -6,65 +6,65 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-
-
 @Entity
 public class Hospedagem {
 
+	@Id
+	private Integer id;
+	@Column
+	private String nomeCliente;
+	private String categoriaQuartos;
+	@Column
+	private Integer quantidadePessoas;
+	private Date entrada;
+	private Date saída;
 
-@Id
-private Integer id;
-@Column
-private String nome;
-private String categoriaQuartos;
-@Column
-private Integer quantidadePessoas ;	
-private Date entrada;
-private Date saída;
-
-
-
-public Integer getId() {
-	return id;
-}
-public void setId(Integer id) {
-	this.id = id;
-}
-public String getNome() {
-		return nome;
+	public Integer getId() {
+		return id;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
-public String getCategoriaQuartos() {
-	return categoriaQuartos;
-}
-public void setCategoriaQuartos(String categoriaQuartos) {
-	this.categoriaQuartos = categoriaQuartos;
-}
-public Integer getQuantidadePessoas() {
-	return quantidadePessoas;
-}
-public void setQuantidadePessoas(Integer quantidadePessoas) {
-	this.quantidadePessoas = quantidadePessoas;
-}
 
-public Date getEntrada() {
-	return entrada;
-}
-public void setEntrada(Date entrada) {
-	this.entrada = entrada;
-}
-public Date getSaída() {
-	return saída;
-}
-public void setSaída(Date saída) {
-	this.saída = saída;
-}
+	public String getCategoriaQuartos() {
+		return categoriaQuartos;
+	}
 
+	public void setCategoriaQuartos(String categoriaQuartos) {
+		this.categoriaQuartos = categoriaQuartos;
+	}
 
+	public Integer getQuantidadePessoas() {
+		return quantidadePessoas;
+	}
 
+	public void setQuantidadePessoas(Integer quantidadePessoas) {
+		this.quantidadePessoas = quantidadePessoas;
+	}
 
-	
+	public Date getEntrada() {
+		return entrada;
+	}
+
+	public void setEntrada(Date entrada) {
+		this.entrada = entrada;
+	}
+
+	public Date getSaída() {
+		return saída;
+	}
+
+	public void setSaída(Date saída) {
+		this.saída = saída;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
 
 }
--- src/br/senai/sc/hoteleclipse/mb/VendaIngressoMb.java
+++ src/br/senai/sc/hoteleclipse/mb/VendaIngressoMb.java
@@ -13,81 +13,47 @@ import br.senai.sc.hoteleclipse.entity.VendaIngresso;
 @ManagedBean
 public class VendaIngressoMb {
 
-	
 	private VendaIngresso vendaIngresso;
 	private VendaIngressoDao dao;
-	private EntityManager entityManager;
 	private List<VendaIngresso> listaVendaIngresso;
-	private Long id;	
-	private String nomeCliente;
-	private Date dataEvento;
-	private String setor;
+
 	public VendaIngresso getVendaIngresso() {
 		return vendaIngresso;
 	}
+
 	public void setVendaIngresso(VendaIngresso vendaIngresso) {
 		this.vendaIngresso = vendaIngresso;
 	}
+
 	public VendaIngressoDao getDao() {
 		return dao;
 	}
+
 	public void setDao(VendaIngressoDao dao) {
 		this.dao = dao;
 	}
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
+
 	public List<VendaIngresso> getListaVendaIngresso() {
 		return listaVendaIngresso;
 	}
+
 	public void setListaVendaIngresso(List<VendaIngresso> listaVendaIngresso) {
 		this.listaVendaIngresso = listaVendaIngresso;
 	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNomeCliente() {
-		return nomeCliente;
-	}
-	public void setNomeCliente(String nomeCliente) {
-		this.nomeCliente = nomeCliente;
-	}
-	public Date getDataEvento() {
-		return dataEvento;
-	}
-	public void setDataEvento(Date dataEvento) {
-		this.dataEvento = dataEvento;
-	}
-	public String getSetor() {
-		return setor;
-	}
-	public void setSetor(String setor) {
-		this.setor = setor;
-	}
-	
+
 	public String salvar() {
-		entityManager.merge(vendaIngresso);
-		return "listagemVendaIngresso";
+		dao.salvar(vendaIngresso);
+		return "listagemvendaIngresso";
 	}
 
 	public String editar(Long ID) {
-		vendaIngresso = entityManager.find(VendaIngresso.class, ID);
-		return "formcadVendaIngresso";
+		vendaIngresso = dao.buscarPorId(ID);
+		return "formcadvendaIngresso";
 	}
 
 	public String excluir(Long ID) {
-		VendaIngresso vendaIngresso = entityManager.getReference(VendaIngresso.class, ID);
-		entityManager.remove(vendaIngresso);
-		listaVendaIngresso = null;
+		vendaIngresso = dao.excluir(ID);
 		return "listagemVendaIngresso";
-		
 	}
-		
 
 }
