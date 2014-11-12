2eaeb3bbffbbf536139202dbcf83328b7f5faa7d
Stephanie <stephanierc16@hotmail.com>
2014-11-12 16:44:31 -0200

    Base.css e EmailUtil feitos

--- src/br/senai/sc/hoteleclipse/util/EmailUtil.java
+++ src/br/senai/sc/hoteleclipse/util/EmailUtil.java
@@ -1,6 +1,5 @@
 package br.senai.sc.hoteleclipse.util;
 
-
 import java.util.Date;
 import java.util.Properties;
 
@@ -17,59 +16,95 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class EmailUtil {
-	
-	public static void enviarEmail(String destinatario, String assunto, String conteudo) throws AddressException, MessagingException {
-		Authenticator authenticator = getAuthenticator();
-		InternetAddress remetente = new InternetAddress(getFrom()); 
-		InternetAddress destinatarioEmail = new InternetAddress(destinatario);
+
+	public static void enviarEmail(String destinatario,
+
+	String assunto, String conteudo) throws AddressException,
+
+	MessagingException {
+		Authenticator authenticator =
+
+		getAuthenticator();
+		InternetAddress remetente = new
+
+		InternetAddress(getFrom());
+		InternetAddress destinatarioEmail = new
+
+		InternetAddress(destinatario);
 		Properties properties = getProperties();
-		
-		Session session = Session.getDefaultInstance(properties, authenticator);
-		
-		Message msg = new MimeMessage(session);
-		
+
+		Session session =
+
+		Session.getDefaultInstance(properties, authenticator);
+
+		Message msg = new MimeMessage
+
+		(session);
+
 		msg.setFrom(remetente);
 		msg.setSentDate(new Date());
-		
-		msg.setRecipient(Message.RecipientType.TO, destinatarioEmail);
+
+		msg.setRecipient
+
+		(Message.RecipientType.TO, destinatarioEmail);
 		msg.setSubject(assunto);
 		msg.setText(conteudo);
-		
-//		session.setDebug(true);
+
+		// session.setDebug(true);
 		Transport.send(msg);
 	}
 
 	private static String getFrom() {
-		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+		ExternalContext context =
+
+		FacesContext.getCurrentInstance().getExternalContext();
 		return context.getInitParameter("usuario");
 	}
 
 	private static Authenticator getAuthenticator() {
-		final ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-		
+		final ExternalContext context =
+
+		FacesContext.getCurrentInstance().getExternalContext();
+
 		return new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(context.getInitParameter("usuario"),
-						context.getInitParameter("senha"));
+				return new
+
+				PasswordAuthentication(context.getInitParameter("usuario"),
+
+				context.getInitParameter("senha"));
 			}
 		};
 	}
 
 	private static Properties getProperties() {
-		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-		
-		Properties properties = new Properties();
-		properties.put("mail.smtp.host", context.getInitParameter("servidor"));
-		properties.put("mail.smtp.port", context.getInitParameter("porta"));
-		properties.put("mail.smtp.auth", context.getInitParameter("auth"));
-		properties.put("mail.smtp.starttls.enable", context.getInitParameter("tls"));
-		properties.setProperty("mail.smtp.ssl.trust", "smtpserver");
-	    properties = (Properties)System.getProperties().clone();   
-	   
+		ExternalContext context =
 
-//		properties.put("mail.smtp.debug", "true");
-		
+		FacesContext.getCurrentInstance().getExternalContext();
+
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host",
+
+		context.getInitParameter("servidor"));
+		properties.put("mail.smtp.port",
+
+		context.getInitParameter("porta"));
+		properties.put("mail.smtp.auth",
+
+		context.getInitParameter("auth"));
+		properties.put("mail.smtp.starttls.enable",
+
+		context.getInitParameter("tls"));
+		properties.setProperty
+
+		("mail.smtp.ssl.trust", "smtpserver");
+		properties = (Properties) System.getProperties
+
+		().clone();
+
+		// properties.put("mail.smtp.debug", "true");
+
 		return properties;
 	}
 }
