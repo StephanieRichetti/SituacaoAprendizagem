a84b2f657dc7945d8b8f919d0b9c18d47a896cb4
bruna_bernadete <bruna_bernadete@SNFNS2EP13VM064.florianopolis.sc.senai.br>
2014-11-18 20:41:21 -0200

    HospedagemDaoTest, ClienteDaoTest e algumas modificações

--- test/br/senai/sc/hoteleclipse/test/ClienteDaoTest.java
+++ test/br/senai/sc/hoteleclipse/test/ClienteDaoTest.java
@@ -1,5 +1,114 @@
 package br.senai.sc.hoteleclipse.test;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import br.senai.sc.hoteleclipse.dao.ClienteDao;
+import br.senai.sc.hoteleclipse.entity.Cliente;
+import br.senai.sc.hoteleclipse.util.Util;
+
 public class ClienteDaoTest {
+
+	private void resetTableCliente() {
+		String query = "truncate cliente;";
+		executeQuery(query);
+	}
+
+	private void insertId1() {
+		String query = "INSERT INTO cliente(id,nome,cpf,endereco,bairro,cidade,estado,email,telefone) VALUES(1,'Cliente 1','cliente')";
+		executeQuery(query);
+	}
+
+	private void insertId10() {
+		String query = "INSERT INTO cliente(nome,email,telefone) VALUES('Cliente 1','clientes cliente 1')";
+
+		for (int i = 2; i <= 10; i++)
+			query = query.concat(",('Cliente " + i + "','Nome " + i + "')");
+		executeQuery(query);
+	}
+
+	private void executeQuery(String query) {
+		EntityManager em = Util.getEntityManager();
+		em.getTransaction().begin();
+
+		em.createNativeQuery(query).executeUpdate();
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	@BeforeClass
+	public static void init() {
+		Util.initFactory();
+	}
+
+	@AfterClass
+	public static void finish() {
+		Util.closeFactory();
+	}
+
+	private EntityManager entityManager;
+	private ClienteDao dao;
+
+	@Before
+	public void begin() {
+		resetTableCliente();
+		entityManager = Util.getEntityManager();
+		entityManager.getTransaction().begin();
+		dao = new ClienteDao(entityManager);
+	}
+
+	@After
+	public void close() {
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManager = null;
+		dao = null;
+	}
+
+	@Test
+	public void testSalvar() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Sara ...");
+		cliente.setCPF("896.356.256-33");
+		cliente.setBairro("Centro");
+		cliente.setCidade("Rio de Janeiro");
+		cliente.setEstado("RJ");
+		cliente.setEmail("João Paulo ...");
+		cliente.setTelefone("32668574");
+		dao.salvar(cliente);
+	}
+
+	@Test
+	public void buscarClientePorId() {
+		insertId1();
+
+		testSalvar();
+		Cliente cliente = dao.buscarPorId(1L);
+		assertNotNull(cliente);
+	}
+
+	@Test
+	public void excluirCliente1() {
+		insertId1();
+		dao.excluir(1L);
+
+	}
+
+	@Test
+	public void listarCliente() {
+		insertId10();
+
+		List<Cliente> clientes = dao.listar();
+		assertEquals(10, clientes.size());
+	}
 
 }
--- test/br/senai/sc/hoteleclipse/test/HospedagemDaoTest.java
+++ test/br/senai/sc/hoteleclipse/test/HospedagemDaoTest.java
@@ -1,5 +1,108 @@
 package br.senai.sc.hoteleclipse.test;
 
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.senai.sc.hoteleclipse.dao.HospedagemDao;
+import br.senai.sc.hoteleclipse.entity.Hospedagem;
+import br.senai.sc.hoteleclipse.util.Util;
+
 public class HospedagemDaoTest {
 
+	private void resetTableHospedagem() {
+		String query = "truncate hospedagem;";
+		executeQuery(query);
+	}
+
+	private void insertId1() {
+		String query = "INSERT INTO hospedagem(id,nome,categoriaQuartos, quantidadePessoas,entrada,saida) VALUES(1,'Hospedagem 1','Simples',2,'17/11/2014','19/11/2014)";
+		executeQuery(query);
+	}
+
+	private void insertId10() {
+		String query = "INSERT INTO hospedagem(nome,categoriaQuartos, quantidadePessoas,entrada,saida) VALUES(2,'Hospedagem 2','Casal',2,'19/11/2014','21/11/2014)";
+
+		for (int i = 2; i <= 10; i++)
+			query = query.concat(",('Stephanie " + i + "','25/12/2014 " + i
+					+ "'29/12/2014" + i + "'Simples" + i + "'2" + i + ")");
+
+		executeQuery(query);
+	}
+
+	private void executeQuery(String query) {
+		EntityManager em = Util.getEntityManager();
+		em.getTransaction().begin();
+
+		em.createNativeQuery(query).executeUpdate();
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	// fim de manipulação do banco
+
+	@BeforeClass
+	public static void init() {
+		Util.initFactory();
+	}
+
+	@AfterClass
+	public static void finish() {
+		Util.closeFactory();
+	}
+
+	private EntityManager entityManager;
+	private HospedagemDao dao;
+
+	@Before
+	public void begin() {
+		resetTableHospedagem();
+		entityManager = Util.getEntityManager();
+		entityManager.getTransaction().begin();
+		dao = new HospedagemDao(entityManager);
+	}
+
+	@After
+	public void close() {
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManager = null;
+		dao = null;
+	}
+
+	@Test
+	public void testSalvar() {
+		Hospedagem hospedagem = new Hospedagem();
+		hospedagem.setNome("Stephanie");
+		hospedagem.setCategoriaQuartos("Simples");
+		hospedagem.setQuantidadePessoas(1);
+	//	hospedagem.setEntrada(entrada);
+	//  hospedagem.setSaida(saida);
+		
+
+		dao.salvar(hospedagem);
+	}
+
+	@Test
+	public void buscarHospedagemPorId() {
+		insertId1();
+
+		testSalvar();
+		Hospedagem hospedagem = dao.buscarPorId(1L);
+		assertNotNull(hospedagem);
+	}
+
+	@Test
+	public void excluirHospedagem1() {
+		insertId1();
+		dao.excluir(1L);
+
+	}
 }
